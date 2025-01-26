08397d7f27bef5b4b60d2ba6af2ad5e2621369a8
unknown <woals5636@naver.com>
2025-01-26 23:47:25 +0900

    Spring Thymeleaf-basic

---
+++ form/src/main/java/hello/item_service/domain/item/ItemType.java
@@ -0,0 +1,12 @@
+package hello.item_service.domain.item;
+
+public enum ItemType {
+
+    BOOK("도서"), FOOD("음식"), ETC("기타");
+
+    private final String description;
+
+    ItemType(String description) {
+        this.description = description;
+    }
+}
--- form/src/main/java/hello/item_service/web/form/FormItemController.java
+++ form/src/main/java/hello/item_service/web/form/FormItemController.java
@@ -4,6 +4,7 @@ import hello.item_service.domain.item.Item;
 import hello.item_service.domain.item.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
+@Slf4j
 public class FormItemController {
 
     private final ItemRepository itemRepository;
@@ -85,6 +87,7 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        log.info("item.open={}", item.getOpen());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
