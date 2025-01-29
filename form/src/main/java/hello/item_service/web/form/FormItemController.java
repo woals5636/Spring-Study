87223101503b2959248700127614b3801f8e734e
unknown <woals5636@naver.com>
2025-01-30 03:24:46 +0900

    Spring Thymeleaf-basic

--- form/src/main/java/hello/item_service/web/form/FormItemController.java
+++ form/src/main/java/hello/item_service/web/form/FormItemController.java
@@ -1,7 +1,9 @@
 package hello.item_service.web.form;
 
+import hello.item_service.domain.item.DeliveryCode;
 import hello.item_service.domain.item.Item;
 import hello.item_service.domain.item.ItemRepository;
+import hello.item_service.domain.item.ItemType;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/form/items")
@@ -19,6 +24,29 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    @ModelAttribute("regions")
+    public Map<String,String> regions(){
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL","서울");
+        regions.put("BUSAN","부산");
+        regions.put("JEJU","제주");
+        return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+        return ItemType.values();
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes(){
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST","빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL","일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW","느린 배송"));
+        return deliveryCodes;
+    }
 
     @GetMapping
     public String items(Model model){
@@ -31,6 +59,7 @@ public class FormItemController {
     public String item(@PathVariable long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item",item);
+
         return "form/item";
     }
 
@@ -40,54 +69,12 @@ public class FormItemController {
         return "form/addForm";
     }
 
-    //    @PostMapping("/add")
-    public String addItemV1(@RequestParam String itemName,
-                            @RequestParam int price,
-                            @RequestParam Integer quantity,
-                            Model model){
-        Item item = new Item();
-        item.setItemName(itemName);
-        item.setPrice(price);
-        item.setQuantity(quantity);
-
-        itemRepository.save(item);
-
-        model.addAttribute("item",item);
-
-        return "form/item";
-    }
-
-    //    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item){
-
-        itemRepository.save(item);
-
-        return "form/item";
-    }
-
-    //    @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item){
-
-        itemRepository.save(item);
-
-        return "form/item";
-    }
-
-    //    @PostMapping("/add")
-    public String addItemV4(Item item){
-        itemRepository.save(item);
-        return "form/item";
-    }
-
-    //    @PostMapping("/add")
-    public String addItemV5(Item item){
-        itemRepository.save(item);
-        return "redirect:/form/items/" + item.getId();
-    }
-
     @PostMapping("/add")
     public String addItemV6(Item item, RedirectAttributes redirectAttributes){
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
+        log.info("item.itemType={}", item.getItemType());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
